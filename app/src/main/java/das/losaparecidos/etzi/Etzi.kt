3eb6705228d00d9fc8817ea4a4e1759c13d0e89c
iker0610 <ikerprogrammer0610@gmail.com>
2022-05-01 20:17:08 +0200

    Firebase Added

--- app/src/main/java/das/losaparecidos/etzi/Etzi.kt
+++ app/src/main/java/das/losaparecidos/etzi/Etzi.kt
@@ -1,9 +1,25 @@
 package das.losaparecidos.etzi
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import com.google.android.material.color.DynamicColors
 import dagger.hilt.android.HiltAndroidApp
 
+
+/*******************************************************************************
+ ****                        Custom Application Class                       ****
+ *******************************************************************************/
+
+/*
+
+This class is needed for Hilt Framework.
+
+I also initialize notification channels here so they are not created each time we send a notification.
+
+Avoiding code  repetition
+
+*/
 
 @HiltAndroidApp
 class Etzi : Application() {
@@ -12,5 +28,52 @@ class Etzi : Application() {
 
         // Apply dynamic color
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+
+        /*------------------------------------------------
+        |          Create Notification Channels          |
+        ------------------------------------------------*/
+
+        // Create the EHU/UPV Information Notification Channel
+        val ehuInformationChannelName = getString(NotificationChannelID.EHU_INFORMATION.nameId)
+        val ehuInformationChannelDescription = getString(NotificationChannelID.EHU_INFORMATION.descriptionId)
+
+        val ehuInformationChannel = NotificationChannel(
+            NotificationChannelID.EHU_INFORMATION.name,
+            ehuInformationChannelName,
+            NotificationManager.IMPORTANCE_HIGH
+        ).apply {
+            description = ehuInformationChannelDescription
+        }
+
+
+        // Get notification manager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+
+        // Register the channels with the system
+        notificationManager.createNotificationChannel(ehuInformationChannel)
     }
+}
+
+
+/*******************************************************************************
+ ****                    Enum Class for Notification IDs                    ****
+ *******************************************************************************/
+
+/*
+Class used to centralize and have better control over application's notification IDs.
+It uses an enum class what gives better readability over the code, and avoids ID mistakes
+
+(In this app we only had one notification, but it's a good practice and eases future expansions and technical debt)
+*/
+
+enum class NotificationChannelID(val nameId: Int, val descriptionId: Int) {
+    EHU_INFORMATION(R.string.ehu_information_channel_name, R.string.ehu_information_channel_description)
+}
+
+enum class NotificationID(val id: Int) {
+    USER_CREATED(0),
+    CORPORATION_NOTIFICATION(1),
+    VISIT_ALARM(2)
 }
---
+++ app/src/main/java/das/losaparecidos/etzi/services/FCMService.kt
@@ -0,0 +1,39 @@
+package das.losaparecidos.etzi.services
+
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import das.losaparecidos.etzi.NotificationChannelID
+import das.losaparecidos.etzi.NotificationID
+import das.losaparecidos.etzi.R
+
+
+/**
+ * Service for handling FCM messages when application is in foreground.
+ */
+class FCMService : FirebaseMessagingService() {
+    override fun onNewToken(token: String) {}
+
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        remoteMessage.notification?.let { notification ->
+
+            Log.d("FCM", "Message Notification Title: ${notification.title}")
+            Log.d("FCM", "Message Notification Body: ${notification.body}")
+
+            // Show user created notification
+            val builder = NotificationCompat.Builder(this, NotificationChannelID.EHU_INFORMATION.name)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle(notification.title)
+                .setContentText(notification.body)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(NotificationID.CORPORATION_NOTIFICATION.id, builder.build())
+            }
+        }
+    }
+}
