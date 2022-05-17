541c53424b40a92325fc53c335ddd8372a0f8c1e
IriaSanMiguel <iria.san.miguel2000@gmail.com>
2022-05-17 18:49:17 +0200

    Ejemplos de asignaturas

---
+++ app/src/main/java/das/losaparecidos/etzi/model/mockdata/SubjectExample.kt
@@ -0,0 +1,172 @@
+package das.losaparecidos.etzi.model.mockdata
+
+import das.losaparecidos.etzi.model.entities.Subject
+import java.time.LocalDate
+
+val subjects = mutableListOf(
+    Subject(
+        "Minería de datos",
+        LocalDate.of(2021, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Optativa",
+        6,
+        4
+    ),
+    Subject(
+        "Desarrollo Avanzado de Software",
+        LocalDate.of(2021, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Optativa",
+        6,
+        4
+    ),
+    Subject(
+        "Desarrollo de Aplicaciones Web Enriquecidas",
+        LocalDate.of(2021, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Optativa",
+        6,
+        4
+    ),
+    Subject(
+        "Técnicas de Inteligencia Artificial",
+        LocalDate.of(2021, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Optativa",
+        6,
+        4
+    ),
+    Subject(
+        "Administración de Sistemas",
+        LocalDate.of(2021, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Optativa",
+        6,
+        4
+    ),
+    Subject(
+        "Análisis Matemático",
+        LocalDate.of(2018, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Básica de rama",
+        6,
+        1
+    ),
+    Subject(
+        "Fundamentos de Tecnología de Computadores",
+        LocalDate.of(2018, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Básica de rama",
+        6,
+        1
+    ),
+    Subject(
+        "Cálculo",
+        LocalDate.of(2018, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Básica de rama",
+        6,
+        1
+    ),
+    Subject(
+        "Programación Modular y Orientación a Objetos",
+        LocalDate.of(2018, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        1
+    ),
+    Subject(
+        "Arquitectura de Computadores",
+        LocalDate.of(2019, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        2
+    ),
+    Subject(
+        "Lenguajes, Computación y Sistemas Inteligentes",
+        LocalDate.of(2019, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        2
+    ),
+    Subject(
+        "Métodos Estadísticos de la Ingeniería",
+        LocalDate.of(2019, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Básica de rama",
+        6,
+        2
+    ),Subject(
+        "Bases de Datos",
+        LocalDate.of(2019, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        2
+    ),Subject(
+        "Introducción a las Redes de Computadores\t",
+        LocalDate.of(2019, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6
+    ),Subject(
+        "Investigación Operativa",
+        LocalDate.of(2019, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Básica de rama",
+        6,
+        2
+    ),Subject(
+        "Diseño de Bases de Datos",
+        LocalDate.of(2020, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        3
+    ),Subject(
+        "Organización de la Producción",
+        LocalDate.of(2020, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        3
+    ),Subject(
+        "Sistemas de Gestión de Seguridad de Sistemas de Información",
+        LocalDate.of(2020, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        3
+    ),Subject(
+        "Gestión de Proyectos",
+        LocalDate.of(2020, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        3
+    ),Subject(
+        "Sistemas de Apoyo a la Decisión",
+        LocalDate.of(2020, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        3
+    ),Subject(
+        "Sistemas Web",
+        LocalDate.of(2020, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Obligatoria",
+        6,
+        3
+    ),Subject(
+        "English for Information Technology",
+        LocalDate.of(2021, 9, 1),
+        "Ingeniería Informática de Gestión y Sistemas de Información",
+        "Optativa",
+        6,
+        4
+    ),
+)
