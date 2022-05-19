4aab4908001a5d7604a468cd9fd8edc8b8ffc512
iker0610 <ikerprogrammer0610@gmail.com>
2022-05-19 13:44:22 +0200

    Added flow return type

--- app/src/main/java/das/losaparecidos/etzi/model/repositories/StudentDataRepository.kt
+++ app/src/main/java/das/losaparecidos/etzi/model/repositories/StudentDataRepository.kt
@@ -2,6 +2,7 @@ package das.losaparecidos.etzi.model.repositories
 
 import das.losaparecidos.etzi.model.entities.Lecture
 import das.losaparecidos.etzi.model.webclients.APIClient
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -11,7 +12,7 @@ class StudentDataRepository @Inject constructor(
     private val apiClient: APIClient,
 ) {
     suspend fun getTimeTable(): Map<String, List<Lecture>> = apiClient.getTimetable().groupBy { it.startDate.date.toString() }
-    suspend fun getTodayTimetable():List<Lecture> = TODO("NOT IMPLEMENTED YET")
+    suspend fun getTodayTimetable(): Flow<List<Lecture>> = TODO("NOT IMPLEMENTED YET")
     suspend fun getTutorials() = apiClient.getTutorials()
     suspend fun getRecord() = apiClient.getRecord()
 }
