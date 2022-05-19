8f7823a742953f2b75cfc69f29c5247bed97a880
iker0610 <ikerprogrammer0610@gmail.com>
2022-05-19 13:44:22 +0200

    Added API to repository

--- app/src/main/java/das/losaparecidos/etzi/model/repositories/StudentDataRepository.kt
+++ app/src/main/java/das/losaparecidos/etzi/model/repositories/StudentDataRepository.kt
@@ -1,5 +1,6 @@
 package das.losaparecidos.etzi.model.repositories
 
+import das.losaparecidos.etzi.model.entities.Lecture
 import das.losaparecidos.etzi.model.webclients.APIClient
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -9,7 +10,8 @@ import javax.inject.Singleton
 class StudentDataRepository @Inject constructor(
     private val apiClient: APIClient,
 ) {
-    suspend fun getTimeTable() = apiClient.getTimetable().groupBy { it.startDate.date.toString() }
+    suspend fun getTimeTable(): Map<String, List<Lecture>> = apiClient.getTimetable().groupBy { it.startDate.date.toString() }
+    suspend fun getTodayTimetable():List<Lecture> = TODO("NOT IMPLEMENTED YET")
     suspend fun getTutorials() = apiClient.getTutorials()
     suspend fun getRecord() = apiClient.getRecord()
 }
