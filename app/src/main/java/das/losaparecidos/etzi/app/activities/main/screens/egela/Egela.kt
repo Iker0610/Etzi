cb52e519ab50f75ec9ffbac083d4eba798bb80dc
IriaSanMiguel <iria.san.miguel2000@gmail.com>
2022-05-22 02:32:13 +0200

    Inicio de sesión automático de egela

--- app/src/main/java/das/losaparecidos/etzi/app/activities/main/screens/egela/Egela.kt
+++ app/src/main/java/das/losaparecidos/etzi/app/activities/main/screens/egela/Egela.kt
@@ -1,14 +1,12 @@
 package das.losaparecidos.etzi.app.activities.main.screens.egela
 
+import android.content.Context
+import android.util.Log
+import android.webkit.CookieManager
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SmallTopAppBar
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
@@ -17,6 +15,27 @@ import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 import das.losaparecidos.etzi.app.activities.main.MainActivityScreens
+import das.losaparecidos.etzi.model.repositories.LoginRepository
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
+import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.util.*
+import it.skrape.core.htmlDocument
+import it.skrape.fetcher.HttpFetcher
+import it.skrape.fetcher.extractIt
+import it.skrape.fetcher.response
+import it.skrape.fetcher.skrape
+import it.skrape.selects.eachTagName
+import it.skrape.selects.html5.input
+import kotlinx.coroutines.*
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.URL
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -35,11 +54,98 @@ fun EgelaScreen(windowSizeClass: WindowSizeClass, onMenuOpen: () -> Unit) {
                 })
         }
     ) { paddingValues ->
-        val egela = rememberWebViewState("https://egela.ehu.eus/")
+
+        LoginRepository
+
+        val datosEgela: JSONObject = runBlocking() { getCookieEgela() }
+        val egela = rememberWebViewState("https://egela.ehu.eus")
 
         WebView(
             egela,
             Modifier.padding(paddingValues)
         )
+
+        val cookie = datosEgela.get("cookie").toString().substring(0, datosEgela.get("cookie").toString().indexOf("/") + 1)
+        CookieManager.getInstance().setCookie("https://egela.ehu.eus", cookie)
     }
+}
+
+@OptIn(InternalAPI::class)
+suspend fun getCookieEgela(): JSONObject = withContext(Dispatchers.IO) {
+
+    //////////////////////////////////////// PRIMERA PETICIÓN ////////////////////////////////////////
+
+    // Obtenemos el loginToken y la cookie inicial
+
+    // Creamos el cliente
+    val client = HttpClient(CIO) {
+        install(HttpTimeout)
+    }
+
+    // Hacemos la petición
+    val responseGetToken: HttpResponse = client.get("https://egela.ehu.eus") {
+        headers {
+            append(HttpHeaders.Accept, "text/html")
+        }
+    }
+
+    // Conseguimos el token
+    val token = getToken(responseGetToken.body<String>())
+    if (token == "fail") {
+        return@withContext JSONObject()
+    }
+
+    val primeraCookie = responseGetToken.headers.get("Set-Cookie")
+
+
+    //////////////////////////////////////// SEGUNDA PETICIÓN ////////////////////////////////////////
+
+
+    val response1: HttpResponse = client.post(
+        "https://egela.ehu.eus/login/index.php"
+    ) {
+        headers {
+            append("Cookie", primeraCookie.toString())
+        }
+        body = FormDataContent(Parameters.build {
+            append("logintoken", token)
+            append("username", "915018")
+            append("password", "")
+        })
+    }
+    println(response1.body<String>())
+    val status1 = response1.status.value
+    val resultado = JSONObject()
+    if (status1 == 303) {
+        val cookieEntera = response1.headers.get("Set-Cookie")
+        val location = response1.headers.get("Location")
+
+        val cookie = cookieEntera.toString().substring(0, cookieEntera.toString().indexOf("/") + 1)
+        //////////////////////////////////////// TERCERA PETICIÓN ////////////////////////////////////////
+        val response2: HttpResponse = client.post(location.toString()) {
+            headers {
+                append("Cookie", cookie)
+            }
+        }
+        if (response2.status.value == 303) { // aqui aparece el testsession (respuesta de la peticion 2)
+            resultado.put("cookie", cookie)
+        }
+    }
+
+    client.close()
+    return@withContext resultado
+}
+
+fun getToken(body: String): String {
+    try {
+        val inputToken = "<input type=\"hidden\" name=\"logintoken\" value=\".*\""
+        val regex = inputToken.toRegex()
+        val token = regex.find(body)
+        if (token != null) {
+            return token.value.substring(46, token.value.length - 1)
+        }
+    } finally {
+
+    }
+    return "fail"
 }
